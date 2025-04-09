970d20e84937c2e19264960b6fcd311972fb1aac
eStrau <ellestrauss01@gmail.com>
2025-04-09 10:59:25 -0400

    redeploying things

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,4 +1,6 @@
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -16,6 +18,30 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        context.Logger.LogInformation($"FunctionHandler received: {input}");
+
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+        /* // Testing from postman, you can use this code to test the function
+        context.Logger.LogInformation($"Body: {json.body}");
+        dynamic body = json.body;
+        context.Logger.LogInformation($"Issue: {body.issue}");
+        context.Logger.LogInformation($"Html: {body.issue.html_url}");
+        string payload = $"{{'text':'Issue Created: {body.issue.html_url}'}}";
+        */
+        
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+    
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
     }
 }
